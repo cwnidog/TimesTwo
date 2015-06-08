b56618b63304e4094a9a410031f5077c59e80daa
cwnidog <cwnidog@comcast.net>
2015-06-08 14:56:47 -0700

    initial commit

---
+++ TimesTwo/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  TimesTwo
+//
+//  Created by John Leonard on 6/8/15.
+//  Copyright (c) 2015 John Leonard. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+  @autoreleasepool {
+      // insert code here...
+      NSLog(@"Hello, World!");
+  }
+    return 0;
+}
